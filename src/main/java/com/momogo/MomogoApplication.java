3c0c92367aab2cdffb3cde3580ae216ae2b1262b
SongDami <sdm07743@gmail.com>
2020-12-23 18:26:35 +0900

    spring boot set

--- src/main/java/com/momogo/MomogoApplication.java
+++ src/main/java/com/momogo/MomogoApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.momogo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MomogoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MomogoApplication.class, args);
 	}
 
 }
